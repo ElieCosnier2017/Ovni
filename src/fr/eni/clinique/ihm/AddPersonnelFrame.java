6bcd2292c19d430642bf1480b064fcefcf7228e6
PatriceCasquette <elie.cosnier2017@campus-eni.fr>
2018-07-02 16:50:14 +0200

    develop : ihm

--- src/fr/eni/clinique/ihm/AddPersonnelFrame.java
+++ src/fr/eni/clinique/ihm/AddPersonnelFrame.java
@@ -1,11 +1,19 @@
 package fr.eni.clinique.ihm;
 
+import fr.eni.clinique.bll.BLLException;
+import fr.eni.clinique.bll.PersonnelManager;
+import fr.eni.clinique.bo.Personnel;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class AddPersonnelFrame extends JInternalFrame {
-    private JTextField textField;
-    private JTextField textField_1;
+public class AddPersonnelFrame extends JInternalFrame implements ActionListener {
+    private JTextField nom;
+    private JTextField motPasse;
+    private JComboBox comboBox;
+    PersonnelManager personnelManager = PersonnelManager.getInstance();
 
     public AddPersonnelFrame() {
         //Ecran avec un titre, redimensionable, fermable, agrandissable, iconifiable
@@ -21,38 +29,74 @@ public class AddPersonnelFrame extends JInternalFrame {
         lblNom.setBounds(57, 44, 46, 14);
         getContentPane().add(lblNom);
 
-        textField = new JTextField();
-        textField.setBounds(164, 41, 201, 20);
-        getContentPane().add(textField);
-        textField.setColumns(10);
+        nom = new JTextField();
+        nom.setBounds(164, 41, 201, 20);
+        getContentPane().add(nom);
+        nom.setColumns(10);
 
         JLabel lblFonction = new JLabel("Fonction");
         lblFonction.setFont(new Font("Calibri", Font.BOLD, 14));
         lblFonction.setBounds(57, 88, 71, 14);
         getContentPane().add(lblFonction);
 
-        textField_1 = new JTextField();
-        textField_1.setColumns(10);
-        textField_1.setBounds(164, 129, 201, 20);
-        getContentPane().add(textField_1);
+        motPasse = new JTextField();
+        motPasse.setColumns(10);
+        motPasse.setBounds(164, 129, 201, 20);
+        getContentPane().add(motPasse);
 
         JLabel lblMotDePasse = new JLabel("Mot de passe");
         lblMotDePasse.setFont(new Font("Calibri", Font.BOLD, 14));
         lblMotDePasse.setBounds(57, 132, 95, 14);
         getContentPane().add(lblMotDePasse);
 
-        JComboBox comboBox = new JComboBox();
+        comboBox = new JComboBox();
+        comboBox.addItem("adm");
+        comboBox.addItem("vet");
+        comboBox.addItem("sec");
         comboBox.setBounds(164, 85, 201, 20);
         getContentPane().add(comboBox);
 
         JButton btnNewButton = new JButton("Valider");
+        btnNewButton.setActionCommand("valider");
+        btnNewButton.addActionListener(this);
         btnNewButton.setBounds(83, 203, 89, 23);
         getContentPane().add(btnNewButton);
 
         JButton btnAnnuler = new JButton("Annuler");
+        btnAnnuler.setActionCommand("annuler");
+        btnAnnuler.addActionListener(this);
         btnAnnuler.setBounds(231, 203, 89, 23);
         getContentPane().add(btnAnnuler);
 
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "valider":
+                System.out.println("yoValider");
+                ajouterPersonnel();
+                break;
+            case "annuler":
+                System.out.println("yoAnnuler");
+                this.dispose();
+                break;
+            default:
+                System.out.println("Probleme e=" + e);
+        }
+    }
+
+    private void ajouterPersonnel() {
+        String name = nom.getText();
+        String motDePasse = motPasse.getText();
+        String role = comboBox.getSelectedItem().toString();
+        Personnel personnel = new Personnel(name,motDePasse,role);
+        try {
+            personnelManager.insertPers(personnel);
+        } catch (BLLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
--- src/fr/eni/clinique/ihm/GeneralFrame.java
+++ src/fr/eni/clinique/ihm/GeneralFrame.java
@@ -383,7 +383,6 @@ public class GeneralFrame extends JFrame implements ActionListener{
 			break;
 
 		case "addpersonnel":
-			System.out.println("addclient");
 			setPersonnel().setVisible(true);
 			break;
 
